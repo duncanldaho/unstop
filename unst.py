60536118c61fd73541cd4084f14d504fab7a02b8
Duncan Idaho <duncanr@posteo.net>
2022-01-18 20:54:38 -0800

    Rewrote script with fewer module dependencies

--- unst.py
+++ unst.py
@@ -1,35 +1,43 @@
-import matplotlib.pylab as plt
-import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-# Load content and style images.
-content_image = plt.imread("data/content.jpeg")
-style_image = plt.imread("data/style.jpeg")
 
-# Convert to float32 np array, add batch dimension, normalize to range [0, 1].
-content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.0
-style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.0
+# Preprocess image data.
+def preprocess(image):
+    """Decode JPEG, cast to float32, normalize to [0, 1], add batch dim."""
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.cast(image, tf.float32) / 255.0
+    image = tf.expand_dims(image, axis=0)
+    return image
 
-# Resizing: It is recommended that the style image is 256 pixels,
-# this size was used when training the style transfer network.
-style_image = tf.image.resize(
-    style_image, (256, 256), preserve_aspect_ratio=True, antialias=True
-)
+
+# Resize image data.
+def resize(image):
+    """Resize image to 256x256."""
+    image = tf.image.resize(
+        image, (256, 256), preserve_aspect_ratio=True, antialias=True
+    )
+    return image
+
+
+# Postprocess image data.
+def postprocess(image):
+    """Remove batch dim., convert to RGB, cast to uint8, encode JPEG data."""
+    image = tf.squeeze(image, axis=None) * 255.0
+    image = tf.cast(image, tf.uint8)
+    image = tf.io.encode_jpeg(image, quality=95)
+    return image
+
+
+# Load raw JPEG data.
+content = tf.io.read_file("data/content.jpeg")
+style = tf.io.read_file("data/style.jpeg")
 
 # Load image stylization module.
 hub_module = hub.load(
     "https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2"
 )
 
-# Stylize image:
-# content_image, style_image, and stylized_image are expected to be 4-D Tensors
-# with shapes [batch_size, image_height, image_width, 3].
-outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
-stylized_image = outputs[0]
-
-# Convert stylized_iamge tensor to numpy array.
-stylized_image_array = stylized_image[0].numpy()
-
-# Save numpy array as image.
-plt.imsave("data/stylized.jpeg", stylized_image_array)
+# Generate and save stylized image.
+outputs = hub_module(preprocess(content), resize(preprocess(style)))
+tf.io.write_file("data/stylized.jpeg", postprocess(outputs))
